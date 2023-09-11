93ac533106358005da183f2c070755db298c23e4
Ahsan9988 <144679621+Ahsan9988@users.noreply.github.com>
2023-09-11 17:05:39 +0500

    Add files via upload

---
+++ Qno1.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+clrscr();
+	printf("Name: Ahsan Raza\n");
+    printf("Fathers Name: Amir Ali \n");
+    printf("Age   : 19 yrs \n");
+    printf("Grade : A+ \n");
+	getch();
+	return 0;
+
+}
